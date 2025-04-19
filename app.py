70ae6711ae795fd1a299502dfa7cc012821fa68e
Harsh Singh <119165659+Harshsinghr@users.noreply.github.com>
2025-04-20 03:07:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def calculate_cost():
     return jsonify({"minimum_cost": cost})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
